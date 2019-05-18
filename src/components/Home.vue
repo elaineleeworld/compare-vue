68da971b748bd5c5a34abe4508103eb4eed0ce72
Elaine Lee <elaine.lee@ff0000.com>
2019-05-18 13:39:54 -0700

    add sass mixins responsive variables

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,25 +4,12 @@
 
 <script>
 export default {
+  // eslint-disable-next-line
   name: "Home"
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1,
-h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style lang='sass' scoped>
+@import '../mq'
 </style>
