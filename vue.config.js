2168f9b5b15cd7a39dcf05bbbf6772bbb2b60558
Elaine Lee <elaine.lee@ff0000.com>
2019-05-19 15:26:49 -0700

    add img loader to vue config

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/compare-vue/' : '/',
+  chainWebpack: (config) => {
+    config.module
+      .rule('images')
+      .use('url-loader')
+      .loader('url-loader')
+      .tap(options => Object.assign(options, { limit: 10240 }));
+  },
 };
